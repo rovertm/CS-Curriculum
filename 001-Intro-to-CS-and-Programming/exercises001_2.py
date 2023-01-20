69d121b847b62b1e2cec6d7005b98b1928e9a86e
rovertm <rovertmckinnon@gmail.com>
2023-01-19 17:31:05 -0800

    Add files via upload

---
+++ 001-Intro-to-CS-and-Programming/exercises001_2.py
@@ -0,0 +1,24 @@
+"""
+
+##############################
+Lecture 2 - Slides - Exercise
+##############################
+
+"""
+
+"""
+
+Break statments 
+
+"""
+
+# break can only be used with for or while loop (not if/else)
+
+# mysum = 0
+# for i in range(5, 11, 2):
+#   mysum += i
+#   if mysum == 5:
+#     break
+#     mysum += 1
+# print(mysum)
+
---
+++ 001-Intro-to-CS-and-Programming/ps001_0.py
@@ -0,0 +1,50 @@
+"""Problem set answers for ps0"""
+"""
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”.
+
+Output: 
+
+Enter number x: 2
+Enter number y: 3
+X**y = 8
+log(x) = 1
+
+# Enter*... values are user inputs
+# Functions are calculated
+
+"""
+
+#####################
+### Problem Set 0 ###
+#####################
+
+import numpy as np
+
+# 1. Asks the user to enter a number “x”
+x = int(input("enter a number: \n"))
+# 2. Asks the user to enter a number “y”
+y = int(input("enter a number: \n"))
+# 3. Prints out number “x”, raised to the power “y”.
+xpowy = x**y
+print("x**y = ", xpowy)
+# 4. Prints out the log (base 2) of “x”.
+lgx = np.log2(x)
+print("log2(x) = ", lgx)
+
+"""
+
+>> follow ups and curiosities...
+
+  * logarithms eli5
+  * https://numpy.org/doc/stable/reference/generated/numpy.log2.html
+
+>> sticking points...
+
+  * running from Shell vs console in .py other than __main__
+  * numpy and .log ++ .log2() methods
+
+
+"""
